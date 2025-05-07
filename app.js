07ec9ce9b3bd054140409644d46637c5ccc583de
Sergio A Cadena <sergio.cadena@unicab.org>
2025-05-07 15:33:34 -0500

    second commit, verficando conection

--- app.js
+++ app.js
@@ -9,4 +9,4 @@ console.log("Hola Mundo desde mi" +curso.nombre); /* muestra el objeto curso */
 
 const array = [1, 2, 3, 4, 5]; /* crea un array */
 array.push(9); /* agrega un elemento al array */
-console.log("y esta es la cantida de datos que tiene mi array: " + array.length + " Datazos"); /* muestra el array */
+console.log("y esta es la cantida de datos que tiene mi array: " + array.length + " Datazos"); /* muestra el array de una*/
